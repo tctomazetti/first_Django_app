fe4047fd36d2f86f1206f619787cb2be834cd8e5
Tiago Tomazetti <tctomazetti@gmail.com>
2024-06-17 10:00:41 -0300

    tutorial part 2

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/id/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/id/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/id/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpRequest
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+from .models import Question
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're voting on question {question_id}.")
+
+def index(request: HttpRequest) -> HttpResponse:
+    last_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "last_question_list": last_question_list
+    }
+    return render(request, "polls/index.html", context)
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
